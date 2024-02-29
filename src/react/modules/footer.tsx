0e4085cf3a03a5d487d21f89a3874e275a899aad
Italo Pereira <italo.pereira@verndale.com>
2024-02-28 22:35:47 -0300

    add footer top

--- src/react/modules/footer.tsx
+++ src/react/modules/footer.tsx
@@ -1,12 +1,52 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { FaCcVisa, FaCcMastercard, FaCcDinersClub, FaPix, FaBarcode } from "react-icons/fa6";
+import { SiAmericanexpress } from "react-icons/si";
+
+import DelayWrapper from "@/react/components/delay-wrapper";
 import "@/sass/modules/_footer.scss";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
+
+        <DelayWrapper>
+          <div className="footer__top">
+            <div className="footer__top__legal">
+              <h4>PIRATARIA É CRIME</h4>
+              <p>A venda do BELLADERMIS só pode ser realizada através deste site. Qualquer outro site onde você encontre este programa é uma FALSIFICAÇÃO e vai contra as leis. Evite falsificações e recuse conteúdos ilegais ou pirateados. NÃO adquira programas parecidos. Não nos responsabilizamos por compras realizadas em outros sites.</p>
+            </div>
+
+            <div className="footer__top__payment">
+              <h4>Formas de Pagamento:</h4>
+              <div className="footer__top__payment__grid">
+                <FaCcVisa />
+                <FaCcMastercard />
+                <FaCcDinersClub />
+                <SiAmericanexpress />
+                <FaPix />
+                <FaBarcode />
+              </div>
+            </div>
+
+            <div className="footer__top__copyright">
+              <Image
+                className="footer__top__animals"
+                src="/images/footer/no-animals-test.png"
+                alt="Não testamos em animais"
+                width={200}
+                height={63}
+              />
+
+              <p className="footer__top__copyright-text">
+                <span>Copyright 2024</span>
+                <span>Todos os direitos reservados ©</span>
+              </p>
+            </div>
+          </div>
+        </DelayWrapper>
 
         <div className="footer__bottom">
           <Image
