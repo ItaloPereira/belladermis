91a1be53fbf97403cfb487ff2c127934c5d17828
Italo Pereira <italo.pereira@verndale.com>
2024-03-04 20:35:18 -0300

    change vturb video

--- src/react/components/vturb.tsx
+++ src/react/components/vturb.tsx
@@ -1,4 +1,4 @@
-// import Image from "next/image";
+import Image from "next/image";
 import Script from "next/script"
 
 import "@/sass/components/_vturb.scss";
@@ -7,29 +7,21 @@ const Vturb = () => {
 
   return (
     <>
-      {/* <div className="vturb" id="vid_65d6aa6297098c0007bd4f4c">
+      <div className="vturb" id="vid_65e64cf924757e00085638c3">
         <Image
           className="vturb__thumbnail"
-          src="https://images.converteai.net/7bfc45fe-b0d2-4dbf-901b-e6f2e94ab583/players/65d6aa6297098c0007bd4f4c/thumbnail.jpg"
+          src="https://images.converteai.net/7bfc45fe-b0d2-4dbf-901b-e6f2e94ab583/players/65e64cf924757e00085638c3/thumbnail.jpg"
           alt="Aperte o Play"
           width={64}
           height={36}
         />
 
-        <div className="vturb__backdrop" id="backdrop_65d6aa6297098c0007bd4f4c" />
-      </div> */}
+        <div className="vturb__backdrop" id="backdrop_65e64cf924757e00085638c3" />
+      </div>
 
-      <script type="text/javascript" id="scr_65e64e72b5d1b9000816f573"></script>
-
-      <Script id="scr_65e64e72b5d1b9000816f573" />
-      <Script id="show-banner">
-        {"var s=document.createElement(\"script\");s.src=\"https://scripts.converteai.net/7bfc45fe-b0d2-4dbf-901b-e6f2e94ab583/ab-test/65e64e72b5d1b9000816f573/player.js\",s.async=!0,document.head.appendChild(s);"}
-      </Script>
+      <Script src="https://scripts.converteai.net/7bfc45fe-b0d2-4dbf-901b-e6f2e94ab583/players/65e64cf924757e00085638c3/player.js" />
     </>
   )
 }
 
 export default Vturb;
-
-
-{/* <script type="text/javascript" id="scr_65e64e72b5d1b9000816f573">var s=document.createElement("script");s.src="https://scripts.converteai.net/7bfc45fe-b0d2-4dbf-901b-e6f2e94ab583/ab-test/65e64e72b5d1b9000816f573/player.js",s.async=!0,document.head.appendChild(s);</script> */}
